d35b1e4dc158154adaebe582d7e68588953ae109
NasriHaithem <mai5haithem@gmail.com>
2021-09-03 23:36:55 +0100

    new login and register pages for instructors and students

--- Bright-angular-project/src/app/components/public/login/login.component.ts
+++ Bright-angular-project/src/app/components/public/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
+
+  studentLoginForm: FormGroup
+  teacherLoginForm: FormGroup
+
+  constructor(private fb: FormBuilder) {
+
+    let studentFormControls = {
+      studentEmail: new FormControl('',[
+        Validators.required,
+        Validators.email
+      ]),
+      studentPassword: new FormControl('',[
+        Validators.required,
+        Validators.minLength(8)
+      ])
+    }
+    let teacherFormControls = {
+      teacherEmail: new FormControl('',[
+        Validators.required,
+        Validators.email
+      ]),
+      teacherPassword: new FormControl('',[
+        Validators.required,
+        Validators.minLength(8)
+      ])
+    }
+
+    this.studentLoginForm = this.fb.group(studentFormControls);
+    this.teacherLoginForm = this.fb.group(teacherFormControls);
+  }
+  //student Getters
+  get studentEmail() { return this.studentLoginForm.get('studentEmail') }
+  get studentPassword() { return this.studentLoginForm.get('studentPassword') }
+  //teacher Getters
+  get teacherEmail() { return this.teacherLoginForm.get('teacherEmail') }
+  get teacherPassword() { return this.teacherLoginForm.get('teacherPassword') }
+  
+
+  studentLogin(){
+    let data = this.studentLoginForm.value;
+    console.log(data);
+  }
+  teacherLogin(){
+    let data = this.teacherLoginForm.value;
+    console.log(data);
+  }
+
+
+  //DOM MANIPULATION
+  addTeacherFormToggledClass() {
+    const box = document.querySelector(".box");
+    box.classList.add("teacher-form-toggled");
+  }
+  removeTeacherFormToggledClass() {
+    const box = document.querySelector(".box");
+    box.classList.remove("teacher-form-toggled");
+  }
+
 
 }
--- Bright-angular-project/src/app/components/public/register/register.component.ts
+++ Bright-angular-project/src/app/components/public/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,107 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
+  
   ngOnInit(): void {
+  }
+
+  studentRegisterForm: FormGroup
+  teacherRegisterForm: FormGroup
+
+  constructor(private fb: FormBuilder) {
+
+    let studentFormControls = {
+      studentFirstname: new FormControl('',[
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2)
+      ]),
+      studentLastname: new FormControl('',[
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2)
+      ]),
+      studentEmail: new FormControl('',[
+        Validators.required,
+        Validators.email
+      ]),
+      studentPassword: new FormControl('',[
+        Validators.required,
+        Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[\.#?!@$%^&*-]).{0,}$"),
+        Validators.minLength(8)
+      ]),
+      studentRepassword: new FormControl('',[
+        Validators.required,
+      ])
+    }
+
+    let teacherFormControls = {
+      teacherFirstname: new FormControl('',[
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2)
+      ]),
+      teacherLastname: new FormControl('',[
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2)
+      ]),
+      teacherEmail: new FormControl('',[
+        Validators.required,
+        Validators.email
+      ]),
+      teacherProfession: new FormControl('',[
+        Validators.required
+      ]),
+      teacherPassword: new FormControl('',[
+        Validators.required,
+        Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[\.#?!@$%^&*-]).{0,}$"),
+        Validators.minLength(8)
+      ]),
+      teacherRepassword: new FormControl('',[
+        Validators.required,
+      ])
+    }
+    this.studentRegisterForm = this.fb.group(studentFormControls);
+    this.teacherRegisterForm = this.fb.group(teacherFormControls);
+  }
+
+  //student Getters
+  get studentFirstname() { return this.studentRegisterForm.get('studentFirstname') }
+  get studentLastname() { return this.studentRegisterForm.get('studentLastname') }
+  get studentEmail() { return this.studentRegisterForm.get('studentEmail') }
+  get studentPassword() { return this.studentRegisterForm.get('studentPassword') }
+  get studentRepassword() { return this.studentRegisterForm.get('studentRepassword') }
+
+  //teacher Getters
+  get teacherFirstname() { return this.teacherRegisterForm.get('teacherFirstname') }
+  get teacherLastname() { return this.teacherRegisterForm.get('teacherLastname') }
+  get teacherEmail() { return this.teacherRegisterForm.get('teacherEmail') }
+  get teacherProfession() { return this.teacherRegisterForm.get('teacherProfession') }
+  get teacherPassword() { return this.teacherRegisterForm.get('teacherPassword') }
+  get teacherRepassword() { return this.teacherRegisterForm.get('teacherRepassword') }
+
+
+  studentRegister(){
+    let data = this.studentRegisterForm.value;
+    console.log(data);   
+  }
+  teacherRegister(){
+    let data = this.teacherRegisterForm.value;
+    console.log(data);   
+  }
+
+
+
+
+  //DOM MANIPULATION
+  addSignUpMode() {
+    const box = document.querySelector(".box");
+    box.classList.add("teacher-form-toggled");
+  }
+  removeSignUpMode() {
+    const box = document.querySelector(".box");
+    box.classList.remove("teacher-form-toggled");
   }
 
 }
